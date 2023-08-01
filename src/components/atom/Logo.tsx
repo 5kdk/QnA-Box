3db91153609a5baa1bf3cc384f391b2094a3207b
5kdk <5kdk.code@gmail.com>
2023-08-01 22:18:45 +0900

    refactor: Logo  atom type 수정

--- src/components/atom/Logo.tsx
+++ src/components/atom/Logo.tsx
@@ -1,22 +1,25 @@
 import { css } from '@emotion/react';
 import MainLogo from '../../assets/qa-logo.svg';
+import Flex from './Flex';
 
 const large = css({
   width: '93px',
 });
+
 const small = css({
   width: '36px',
 });
 
 interface LogoProps {
   size: 'lg' | 'sm';
+  onClick?: () => void;
 }
 
 const Logo = ({ size, ...rest }: LogoProps) => {
   return (
-    <div css={size === 'lg' ? large : small} {...rest}>
+    <Flex css={size === 'lg' ? large : small} {...rest}>
       <img src={MainLogo} alt="로고 이미지" />
-    </div>
+    </Flex>
   );
 };
 

3e6206e1e04b5f05869b65daf8ec2b4833b37605
YUNH7 <yunny126@gmail.com>
2023-08-01 21:18:46 +0900

    design: Button flex 속성 설정

--- src/components/atom/Button.tsx
+++ src/components/atom/Button.tsx
@@ -7,31 +7,26 @@ export interface cssProps {
   borderColor?: string;
   icon?: ReactNode;
 }
-const buttonCss = {
-  base: (props: cssProps) =>
-    css({
-      minWidth: '6rem',
-      padding: '10px',
-      borderRadius: '10px',
-      fontWeight: 'bold',
-      color: props.color,
-      backgroundColor: props.bgColor,
-      borderColor: props.borderColor || props.bgColor,
-    }),
-  flex: css({
-    display: 'inline-flex',
-    justifyContent: 'space-between',
+const buttonCss = (props: cssProps) =>
+  css({
+    display: 'flex',
+    justifyContent: props.icon ? 'space-between' : 'stretch',
     alignItems: 'center',
-  }),
-};
-
+    minWidth: '6rem',
+    padding: '10px',
+    borderRadius: '10px',
+    fontWeight: 'bold',
+    color: props.color,
+    backgroundColor: props.bgColor,
+    borderColor: props.borderColor || props.bgColor,
+  });
 export interface ButtonProps extends cssProps {
   text: string;
   onClick: () => void;
 }
 const Button = ({ text, color, bgColor, borderColor, icon, onClick }: ButtonProps) => {
   return (
-    <button css={[buttonCss.base({ color, bgColor, borderColor }), icon && buttonCss.flex]} onClick={onClick}>
+    <button css={buttonCss({ color, bgColor, borderColor, icon })} onClick={onClick}>
       {icon}
       {text}
     </button>

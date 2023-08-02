2343d1ec9564fa17ed3c39602053853e561fe301
YUNH7 <yunny126@gmail.com>
2023-08-02 14:02:05 +0900

    design: 인풋 focus 시 테두리 강조

--- src/components/molecules/Question.tsx
+++ src/components/molecules/Question.tsx
@@ -6,41 +6,48 @@ import Flex from '../atom/Flex';
 import Avartar from '../atom/Avartar';
 import loading from '../../assets/images/loading.png';
 
-const inputCss = css({
-  gap: '9px',
-  backgroundColor: '#F0F0F0',
-  borderRadius: '50px',
-  padding: '9px',
-  '> input': {
+const questionCss = {
+  wrapper: css({
+    position: 'fixed',
+    bottom: '0',
+    zIndex: '10',
+    width: '100%',
+    gap: '10px',
+    padding: '20px',
+    backgroundColor: 'white',
+    boxShadow: '0px -10px 10px 1px rgba(0, 0, 0, 0.10)',
+  }),
+  inputBox: css({
+    display: 'flex',
+    alignItems: 'center',
+    gap: '9px',
+    backgroundColor: '#F0F0F0',
+    borderRadius: '50px',
+    padding: '9px',
+    border: '2px solid #F0F0F0',
+    '&:focus-within': {
+      borderColor: '#1C56FC',
+    },
+  }),
+  input: css({
     width: '100%',
     border: 'none',
     backgroundColor: 'inherit',
     fontSize: '15px',
     outline: 'none',
-  },
-});
-
-const questionCss = css({
-  position: 'fixed',
-  bottom: '0',
-  zIndex: '10',
-  width: '100%',
-  gap: '10px',
-  padding: '20px',
-  backgroundColor: 'white',
-  boxShadow: '0px -10px 10px 1px rgba(0, 0, 0, 0.10)',
-});
+  }),
+};
 
 const Question = () => {
   const [isAnonymous, setIsAnonymous] = useState(false);
   const [question, setQuestion] = useState('');
 
   return (
-    <Flex css={questionCss} flexDirection="column">
-      <Flex css={inputCss} alignItems="center">
+    <Flex css={questionCss.wrapper} flexDirection="column">
+      <label css={questionCss.inputBox}>
         <Avartar src={loading} size="sm" />
-        <input placeholder="무엇이 궁금한가요?" onChange={e => setQuestion(e.target.value)} />
-      </Flex>
+        <input css={questionCss.input} placeholder="무엇이 궁금한가요?" onChange={e => setQuestion(e.target.value)} />
+      </label>
       <Flex justifyContent="space-between" alignItems="center">
         <Toggler selected={isAnonymous} setSelected={setIsAnonymous} text="익명으로 질문하기" />
         <Button text="질문 등록" color="white" bgColor="black" onClick={() => console.log(question)} />

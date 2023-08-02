72db3d18d0391ef8c4369d4d9f6a2dc31e467b8f
YUNH7 <yunny126@gmail.com>
2023-08-02 13:28:36 +0900

    feat: create Question input 상태 생성

--- src/components/molecules/Question.tsx
+++ src/components/molecules/Question.tsx
@@ -33,16 +33,17 @@ const questionCss = css({
 
 const Question = () => {
   const [isAnonymous, setIsAnonymous] = useState(false);
+  const [question, setQuestion] = useState('');
 
   return (
     <Flex css={questionCss} flexDirection="column">
       <Flex css={inputCss} alignItems="center">
         <Avartar src={loading} size="sm" />
-        <input placeholder="무엇이 궁금한가요?" />
+        <input placeholder="무엇이 궁금한가요?" onChange={e => setQuestion(e.target.value)} />
       </Flex>
       <Flex justifyContent="space-between" alignItems="center">
         <Toggler selected={isAnonymous} setSelected={setIsAnonymous} text="익명으로 질문하기" />
-        <Button text="질문 등록" color="white" bgColor="black" onClick={() => {}} />
+        <Button text="질문 등록" color="white" bgColor="black" onClick={() => console.log(question)} />
       </Flex>
     </Flex>
   );

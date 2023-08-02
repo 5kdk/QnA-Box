a9d292cf45f3a6156ef6e55d7b99f8474862e5dc
5kdk <5kdk.code@gmail.com>
2023-08-02 13:10:57 +0900

    feat: 포스트 시간 계산 함수 구현

--- src/components/molecules/Item.tsx
+++ src/components/molecules/Item.tsx
@@ -26,23 +26,58 @@ const subTextStyle = css({
   fontSize: '12px',
   color: '#8E8E8E',
   fontWeight: 400,
+  marginRight: '5px',
+});
+
+const menuWrapperStyle = css({
+  position: 'relative',
 });
 
 interface QItemProps {
-  imgUrl?: string;
   userName: string;
+  postTime: string;
   text: string;
+  imgUrl?: string;
   isLike?: boolean;
   like?: number;
 }
 
-const Item = ({ imgUrl, userName, text, isLike, like }: QItemProps) => {
+const displayTimeAgo = (postTimestamp: string): string => {
+  const currentTime = new Date();
+  const postTime = new Date(postTimestamp);
+  const timeDifferenceInSeconds = Math.floor((currentTime.getTime() - postTime.getTime()) / 1000);
+
+  const SECONDS_IN_MINUTE = 60;
+  const SECONDS_IN_HOUR = 3600;
+  const SECONDS_IN_DAY = 86400;
+  const SECONDS_IN_WEEK = 604800;
+  const SECONDS_IN_YEAR = 31536000;
+
+  if (timeDifferenceInSeconds < SECONDS_IN_MINUTE) {
+    return `${timeDifferenceInSeconds}s`;
+  } else if (timeDifferenceInSeconds < SECONDS_IN_HOUR) {
+    return `${Math.floor(timeDifferenceInSeconds / SECONDS_IN_MINUTE)}m`;
+  } else if (timeDifferenceInSeconds < SECONDS_IN_DAY) {
+    return `${Math.floor(timeDifferenceInSeconds / SECONDS_IN_HOUR)}h`;
+  } else if (timeDifferenceInSeconds < SECONDS_IN_WEEK) {
+    return `${Math.floor(timeDifferenceInSeconds / SECONDS_IN_DAY)}d`;
+  } else if (timeDifferenceInSeconds < SECONDS_IN_YEAR) {
+    return `${Math.floor(timeDifferenceInSeconds / SECONDS_IN_WEEK)}w`;
+  } else {
+    return `${Math.floor(timeDifferenceInSeconds / SECONDS_IN_YEAR)}y`;
+  }
+};
+
+const Item = ({ imgUrl, userName, postTime, text, isLike, like }: QItemProps) => {
   return (
     <Flex css={wrapperStyle} justifyContent="space-between">
       <Avartar size="sm" src={imgUrl} />
       <Flex flexDirection="column">
-        <Flex justifyContent="space-between" alignItems="center">
+        <Flex justifyContent="space-between" alignItems="flex-start">
           <span css={nameStyle}>{userName}</span>
+          <Flex alignItems="center" css={menuWrapperStyle}>
+            {postTime && <span css={subTextStyle}>{displayTimeAgo(postTime)}</span>}
+          </Flex>
         </Flex>
         <p css={textStyle}>{text}</p>
         <Flex alignItems="center" css={css({ gap: '5px' })}>

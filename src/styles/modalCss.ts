854516ae30025634c77dde98868d989ea295314d
YUNH7 <yunny126@gmail.com>
2023-08-08 16:46:20 +0900

    refactor: modalCss 파일 분리

---
+++ src/styles/modalCss.ts
@@ -0,0 +1,11 @@
+import { css } from '@emotion/react';
+
+export default css`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  width: 100%;
+  height: 100vh;
+  background-color: var(--modal);
+`;

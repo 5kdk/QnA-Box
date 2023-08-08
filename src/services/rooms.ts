54e535e0b10138d0c64d74f37f42ce1f368f17ae
5kdk <5kdk.code@gmail.com>
2023-08-09 06:05:16 +0900

    feat: 자신의 qna room을 가져오는 함수 구현

--- src/services/rooms.ts
+++ src/services/rooms.ts
@@ -1,4 +1,4 @@
-import { addDoc, arrayUnion, collection, doc, updateDoc } from 'firebase/firestore';
+import { addDoc, arrayUnion, collection, doc, getDocs, query, updateDoc, where } from 'firebase/firestore';
 import { auth, db } from './firebase';
 import { USERS_COLLECTION_NAME, ROOMS_COLLECTION_NAME } from '../constants/collectionNames';
 
@@ -8,6 +8,19 @@ interface FormData {
   activation: boolean;
   anonymous: boolean;
 }
+
+interface Room {
+  id: string;
+  title: string;
+  owner: string;
+  ownerId: string;
+  activation: boolean;
+  anonymous: boolean;
+  createdAt: number;
+  description: string;
+}
+
+const OWNER_UID = 'ownerUid';
 
 const updateUserQnaRooms = async (userUid: string, roomId: string): Promise<void> => {
   const userDocRef = doc(db, USERS_COLLECTION_NAME, userUid);
@@ -24,4 +37,24 @@ export const createQnaRoom = async (formData: FormData) => {
   const qnaRoomsCollection = collection(db, ROOMS_COLLECTION_NAME);
   const qnaRoomDocRef = await addDoc(qnaRoomsCollection, newData);
   await updateUserQnaRooms(user.uid, qnaRoomDocRef.id);
+};
+
+export const getMyQnaRooms = async () => {
+  const user = auth.currentUser;
+  if (!user) return;
+
+  const qnaRoomsCollectionRef = collection(db, ROOMS_COLLECTION_NAME);
+  const ownerUidFilter = where(OWNER_UID, '==', user.uid);
+  const q = query(qnaRoomsCollectionRef, ownerUidFilter);
+  const querySnapshot = await getDocs(q);
+
+  const rooms: Room[] = querySnapshot.docs.map(
+    doc =>
+      ({
+        id: doc.id,
+        ...doc.data(),
+      }) as Room,
+  );
+
+  return rooms;
 };

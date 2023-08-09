7478850cb93a5e4c6741a694229e2e4dcc61dfe2
5kdk <5kdk.code@gmail.com>
2023-08-10 07:20:17 +0900

    feat: 검색기능 구현 제목, 소유자 검색

--- src/components/BoxList/Board.tsx
+++ src/components/BoxList/Board.tsx
@@ -4,7 +4,7 @@ import { BoxItem } from '.';
 import { MainFilter } from '../pages/BoxList';
 import { Flex, Text } from '../atom';
 import { ItemWrapper } from '../molecules';
-import { filterState } from '../../jotai/atom';
+import { filterState, searchInputState } from '../../jotai/atom';
 import { useAtomValue } from 'jotai';
 
 const WrapperCss = css`
@@ -18,18 +18,35 @@ interface BoxListBodyProps {
 const Board = ({ boxFilter }: BoxListBodyProps) => {
   const boxList = useMyListQuery(boxFilter);
   const filter = useAtomValue(filterState);
+  const searchInput = useAtomValue(searchInputState);
 
-  const filteredBoxList =
-    filter === '최신순'
-      ? boxList.sort((a, b) => a.createdAt - b.createdAt)
-      : boxList.sort((a, b) => b.createdAt - a.createdAt);
+  const filteredBoxList = () => {
+    const sorted =
+      filter === '최신순'
+        ? boxList.sort((a, b) => a.createdAt - b.createdAt)
+        : boxList.sort((a, b) => b.createdAt - a.createdAt);
+
+    if (searchInput) {
+      const searchString = searchInput.trim().toLowerCase();
+      const regex = new RegExp(searchString);
+      return sorted.filter(box => regex.test(box.title.toLowerCase()) || regex.test(box.owner.toLowerCase()));
+    }
+
+    return sorted;
+  };
 
   return (
     <ItemWrapper>
-      {filteredBoxList?.length !== 0 ? (
-        filteredBoxList.map(({ boxId, owner, ownerUid, title, description }) => (
-          <BoxItem title={title} owner={owner} text={description} ownerUid={ownerUid} key={boxId} />
-        ))
+      {boxList?.length !== 0 ? (
+        filteredBoxList().length !== 0 ? (
+          filteredBoxList().map(({ boxId, owner, ownerUid, title, description }) => (
+            <BoxItem title={title} owner={owner} text={description} ownerUid={ownerUid} key={boxId} />
+          ))
+        ) : (
+          <Flex justifyContent="center" alignItems="center" css={WrapperCss}>
+            <Text>검색 결과가 없습니다.</Text>
+          </Flex>
+        )
       ) : (
         <Flex justifyContent="center" alignItems="center" css={WrapperCss}>
           <Text>{boxFilter === 'joined' ? '아직 참여한 Box가 없습니다.' : '새로운 QnA Box를 만들어 보세요!'}</Text>

9f7a0064de106e39b8588230fdc16dc89f429017
5kdk <5kdk.code@gmail.com>
2023-08-10 05:20:19 +0900

    feat: useMyListQuery 커스텀 훅 구현

---
+++ src/hooks/query/useMyListQuery.ts
@@ -0,0 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
+import { MainFilter } from '../../components/pages/BoxList';
+import { useAtomValue } from 'jotai';
+import { userState } from '../../jotai/atom';
+import { getMyQnaBoxes, getQnaBoxesById } from '../../services/boxes';
+
+const staleTime = 3000;
+
+const useMyListQuery = (boxFilter: MainFilter) => {
+  const { joinedBoxes } = useAtomValue(userState);
+
+  const queryFn = boxFilter === 'joined' ? () => getQnaBoxesById(joinedBoxes) : getMyQnaBoxes;
+
+  const { data } = useQuery({
+    queryKey: ['box', boxFilter],
+    queryFn,
+    staleTime,
+  });
+
+  return data || [];
+};
+
+export default useMyListQuery;

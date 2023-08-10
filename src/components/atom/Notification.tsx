d04d2625f1d1e05f661fbf8e06ddaeb57b3cf0c5
YUNH7 <yunny126@gmail.com>
2023-08-10 14:12:27 +0900

    design: toast 스타일 변경(폰트크기, 색상, 애니메이션)

--- src/components/atom/Notification.tsx
+++ src/components/atom/Notification.tsx
@@ -1,18 +1,30 @@
 import { useEffect } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
+import { useLocation } from 'react-router-dom';
 import { useAtomValue } from 'jotai';
+import { Slide, ToastContainer, toast } from 'react-toastify';
+import { css } from '@emotion/react';
 import { toastErrorState } from '../../jotai/atom';
 import 'react-toastify/dist/ReactToastify.css';
 
+const notificationCss = css`
+  font-size: 13px;
+`;
+
 const Notification = () => {
   const errors = useAtomValue(toastErrorState);
+  const { pathname } = useLocation();
 
   useEffect(() => {
+    toast.dismiss();
     if (Array.isArray(errors)) errors.forEach(error => error && toast.info(error));
     else if (errors) toast.info(errors);
   }, [errors]);
 
-  return <ToastContainer newestOnTop style={{ fontSize: 14 }} />;
+  useEffect(() => {
+    toast.dismiss();
+  }, [pathname]);
+
+  return <ToastContainer newestOnTop css={notificationCss} position="top-center" autoClose={3000} transition={Slide} />;
 };
 
 export default Notification;

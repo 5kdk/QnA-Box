64d729b1956db454bee06e30d5d7f4fbaf10a2a4
YUNH7 <yunny126@gmail.com>
2023-08-10 23:04:57 +0900

    refactor: User react-query 적용(suspense, error)

---
+++ src/components/User/UserProfile.tsx
@@ -0,0 +1,17 @@
+import { useQuery } from '@tanstack/react-query';
+import { UserInfo } from '.';
+import { getProfile } from '../../services/profile';
+import { useParams } from 'react-router-dom';
+
+const UserProfile = () => {
+  const { uid } = useParams();
+  const { data } = useQuery({
+    queryKey: ['user', uid],
+    queryFn: () => getProfile(uid),
+    staleTime: 3000,
+  });
+
+  return <UserInfo {...data} />;
+};
+
+export default UserProfile;

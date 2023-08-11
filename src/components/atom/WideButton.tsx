e920d682289ce88d54caba97322e675cce88aa3d
YUNH7 <yunny126@gmail.com>
2023-08-11 23:28:47 +0900

    fix: Button type props 생성(기본값 button)

--- src/components/atom/WideButton.tsx
+++ src/components/atom/WideButton.tsx
@@ -1,16 +1,17 @@
 import Button, { ButtonProps } from './Button';
 
-const WideButton = ({ text, color, bgColor, borderColor, icon, onClick }: ButtonProps) => {
+const WideButton = ({ text, color, bgColor, borderColor, icon, type, onClick }: ButtonProps) => {
   return (
     <Button
       minWidth="320px"
       padding="16px"
-      text={text}
-      fontSize="16px"
       color={color}
       bgColor={bgColor}
       borderColor={borderColor}
+      fontSize="16px"
       icon={icon}
+      text={text}
+      type={type}
       onClick={onClick}
     />
   );

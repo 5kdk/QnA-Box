b93f8299611405e948da0bae38f506bfbcbd4837
5kdk <5kdk.code@gmail.com>
2023-08-11 16:12:09 +0900

    style: 보더레디우스 수정

--- src/styles/buttonCss.ts
+++ src/styles/buttonCss.ts
@@ -7,10 +7,11 @@ export default (props: cssProps) => css`
   align-items: center;
   min-width: ${props.minWidth || '96px'};
   padding: ${props.padding || '10px'};
-  border-radius: 10px;
+  border-radius: 5px;
   border-width: 2px;
   border-style: solid;
   border-color: ${props.borderColor || props.bgColor};
+  font-size: ${props.fontSize || undefined};
   font-weight: ${props.fontWeight || 'bold'};
   color: ${props.color};
   background-color: ${props.bgColor};

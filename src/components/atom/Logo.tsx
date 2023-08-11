bcaf37ff09870b55f54b8a6c649ba05506469193
5kdk <5kdk.code@gmail.com>
2023-08-11 16:11:56 +0900

    feat: logo xl 사이즈 추가

--- src/components/atom/Logo.tsx
+++ src/components/atom/Logo.tsx
@@ -1,8 +1,12 @@
 import { css } from '@emotion/react';
 import MainLogo from '../../assets/qa-logo.svg';
+import ReverseLogo from '../../assets/qa-logo-white.svg';
 import { Flex } from './';
 
 const LogoCss = {
+  xlarge: css`
+    width: 120px;
+  `,
   large: css`
     width: 93px;
   `,
@@ -12,14 +16,15 @@ const LogoCss = {
 };
 
 interface LogoProps {
-  size: 'lg' | 'sm';
+  size: 'xl' | 'lg' | 'sm';
+  reverse?: boolean;
   onClick?: () => void;
 }
 
-const Logo = ({ size, ...rest }: LogoProps) => {
+const Logo = ({ size, reverse, ...rest }: LogoProps) => {
   return (
-    <Flex css={size === 'lg' ? LogoCss.large : LogoCss.small} {...rest}>
-      <img src={MainLogo} alt="로고 이미지" />
+    <Flex css={size === 'xl' ? LogoCss.xlarge : size === 'lg' ? LogoCss.large : LogoCss.small} {...rest}>
+      <img src={reverse ? ReverseLogo : MainLogo} alt="로고 이미지" />
     </Flex>
   );
 };

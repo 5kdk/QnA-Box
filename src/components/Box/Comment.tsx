a2d7de72d890b0aaa251feea60e8b55de4e53c9c
operat04 <operat04@naver.com>
2023-08-12 15:37:19 +0900

    Refoctor : displayTimeAgo 함수 분리

--- src/components/Box/Comment.tsx
+++ src/components/Box/Comment.tsx
@@ -6,6 +6,7 @@ import { Reply } from '@emotion-icons/boxicons-regular/Reply';
 import EditCommentForm from './EditCommentForm';
 import { useQuery } from '@tanstack/react-query';
 import { getProfile } from '../../services/profile';
+import { displayTimeAgo } from '../../utils';
 
 const boxItemCss = {
   wrapper: (reply: boolean) => css`
@@ -79,31 +80,6 @@ type Profile = {
   uid: string;
 };
 
-const displayTimeAgo = (postTimestamp: number): string => {
-  const currentTime = new Date();
-  const postTime = new Date(postTimestamp);
-  const timeDifferenceInSeconds = Math.floor((currentTime.getTime() - postTime.getTime()) / 1000);
-
-  const SECONDS_IN_MINUTE = 60;
-  const SECONDS_IN_HOUR = 3600;
-  const SECONDS_IN_DAY = 86400;
-  const SECONDS_IN_WEEK = 604800;
-  const SECONDS_IN_YEAR = 31536000;
-
-  if (timeDifferenceInSeconds < SECONDS_IN_MINUTE) {
-    return `${timeDifferenceInSeconds}s`;
-  } else if (timeDifferenceInSeconds < SECONDS_IN_HOUR) {
-    return `${Math.floor(timeDifferenceInSeconds / SECONDS_IN_MINUTE)}m`;
-  } else if (timeDifferenceInSeconds < SECONDS_IN_DAY) {
-    return `${Math.floor(timeDifferenceInSeconds / SECONDS_IN_HOUR)}h`;
-  } else if (timeDifferenceInSeconds < SECONDS_IN_WEEK) {
-    return `${Math.floor(timeDifferenceInSeconds / SECONDS_IN_DAY)}d`;
-  } else if (timeDifferenceInSeconds < SECONDS_IN_YEAR) {
-    return `${Math.floor(timeDifferenceInSeconds / SECONDS_IN_WEEK)}w`;
-  } else {
-    return `${Math.floor(timeDifferenceInSeconds / SECONDS_IN_YEAR)}y`;
-  }
-};
 const Comment = ({
   setReplyComment,
   setReplyUser,
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { default as errorObjToString } from './errorObjToString';
 export { default as extractUsernameFromEmail } from './extractUsernameFromEmail';
 export { default as requiredFormValue } from './requiredFormValue';
+export { default as displayTimeAgo } from './displayTimeAgo';

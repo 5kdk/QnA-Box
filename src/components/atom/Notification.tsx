9c6298111e1af054e7dfe144f0d191abb059b8ad
YUNH7 <yunny126@gmail.com>
2023-08-14 21:52:09 +0900

    fix: 경로 바뀌어도 toast 메세지 남아 있도록 수정

--- src/components/atom/Notification.tsx
+++ src/components/atom/Notification.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
 import { useAtomValue } from 'jotai';
 import { Slide, ToastContainer, toast } from 'react-toastify';
 import { css } from '@emotion/react';
@@ -12,17 +11,12 @@ const notificationCss = css`
 
 const Notification = () => {
   const errors = useAtomValue(toastErrorState);
-  const { pathname } = useLocation();
 
   useEffect(() => {
     toast.dismiss();
     if (Array.isArray(errors)) errors.forEach(error => error && toast.info(error));
     else if (errors) toast.info(errors);
   }, [errors]);
-
-  useEffect(() => {
-    toast.dismiss();
-  }, [pathname]);
 
   return <ToastContainer newestOnTop css={notificationCss} position="top-center" autoClose={3000} transition={Slide} />;
 };
